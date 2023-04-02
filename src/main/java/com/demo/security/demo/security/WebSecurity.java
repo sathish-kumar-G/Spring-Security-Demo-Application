12ced099a7b81874480d87bdbc4c21e47f806d14
Sathish <sathishkumar.ganesan1997@gmail.com>
2023-04-02 21:24:09 +0530

    first commit

--- src/main/java/com/demo/security/demo/security/WebSecurity.java
+++ src/main/java/com/demo/security/demo/security/WebSecurity.java
@@ -1,12 +1,15 @@
 package com.demo.security.demo.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurity extends WebSecurityConfigurerAdapter {
-
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+    }
 }
